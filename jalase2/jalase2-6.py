a0466eef9b1c6e7f26186d7edaf92f77999ebe13
rootamin <osalimohammadamin@gmail.com>
2024-08-12 21:23:08 +0330

    added calculator while

--- jalase2/jalase2-6.py
+++ jalase2/jalase2-6.py
@@ -1,20 +1,26 @@
 # a simple, running calculator
-first_number = int(input("First number: "))
-second_number = int(input("Second number: "))
+while True:
+    first_number = int(input("First number: "))
+    second_number = int(input("Second number: "))
 
-operation_mode = input("What operation do you want? (+ - x %): ")
+    operation_mode = input("What operation do you want? (+ - x %): ")
 
-if operation_mode == "+":
-    result = first_number + second_number
-elif operation_mode == "-":
-    result = first_number - second_number
-elif operation_mode == "x":
-    result = first_number * second_number
-elif operation_mode == "%":
-    result = first_number / second_number
+    if operation_mode == "+":
+        result = first_number + second_number
+    elif operation_mode == "-":
+        result = first_number - second_number
+    elif operation_mode == "x":
+        result = first_number * second_number
+    elif operation_mode == "%":
+        result = first_number / second_number
 
-else:
-    print("Invalid operation.")
-    exit()
+    else:
+        print("Invalid operation.")
+        exit()
 
-print(f"{first_number} {operation_mode} {second_number} = {result}")
+    print(f"{first_number} {operation_mode} {second_number} = {result}")
+    want_out = input("Do you wanna do another operation? (y or n) ")
+    print('\n===================')
+
+    if want_out == "n":
+        break
